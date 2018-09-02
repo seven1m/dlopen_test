305973ddad019993bdd9eed4bb4dfa240b04bf7b
Tim Morgan <tim@timmorgan.org>
2018-09-01 21:32:27 -0500

    Add comment about gcc attribute

--- lib.c
+++ lib.c
@@ -1,5 +1,7 @@
 #include "stdio.h"
 
+// https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html
+// gcc only? what happens with clang?
 void __attribute__ ((constructor)) lib_loaded(void);
 
 void lib_loaded(void) {
